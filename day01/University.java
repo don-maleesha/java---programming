f897944f0350d8f82499be6e63801e80cd18ad18
donMaleesha <lochanamt@gmail.com>
2024-06-16 13:09:46 +0530

    Practical 01

---
+++ day01/University.java
@@ -0,0 +1,6 @@
+class University {
+    public static void main(String[] args) {
+        System.out.println("My name is Lochana.");
+        System.out.println("I am an undergraduate at UCSC");
+    }
+}
