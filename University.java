33e305e591edae1a5c8abbd9b10fbf99e381ad0d
Lochana Thilakarathne <140266782+don-maleesha@users.noreply.github.com>
2024-06-16 13:07:43 +0530

    Delete University.java

--- University.java
+++
@@ -1,6 +0,0 @@
-class University {
-    public static void main(String[] args) {
-        System.out.println("My name is Lochana.");
-        System.out.println("I am an undergraduate at UCSC");
-    }
-}
