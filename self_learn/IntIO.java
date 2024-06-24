715359ff633d1f0984d24898d98702290e492ade
donMaleesha <lochanamt@gmail.com>
2024-06-24 20:27:45 +0530

    my self learnings

---
+++ self_learn/IntIO.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IntIO {
+    
+    public static void main(String[] args) {
+        
+        Scanner number = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int input = number.nextInt();
+
+        System.out.println("You entered: " + input);
+        
+    }
+}
