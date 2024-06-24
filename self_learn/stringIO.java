75b9867f4063632cffb0b87fdbba7dccbfe1ecbf
donMaleesha <lochanamt@gmail.com>
2024-06-24 20:13:25 +0530

    my self learnings

---
+++ self_learn/stringIO.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class stringIO {
+    
+    public static void main(String[] args){
+
+        Scanner sentence = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String input = sentence.nextLine();
+
+        System.out.println("You entered: " + input);
+
+    }
+}
