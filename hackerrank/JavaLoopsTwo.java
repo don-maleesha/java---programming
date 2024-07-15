b3c7cb27486751ca49bdbd7d7596e1dae73c1517
donMaleesha <lochanamt@gmail.com>
2024-07-15 21:38:06 +0530

    hackerRan tasks

--- hackerrank/JavaLoopsTwo.java
+++ hackerrank/JavaLoopsTwo.java
@@ -1,17 +1,37 @@
+
+
 import java.util.*;
 
 class JavaLoopsTwo{
+
     public static void main(String []argh){
 
         Scanner in = new Scanner(System.in);
 
-        int t=in.nextInt();
+        int t = in.nextInt(); // number of queries
 
-        for(int i=0;i<t;i++){
+        //for each query
+        for(int i = 0; i < t; i++){
 
+            //get values of a, b, and n
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
+
+            //for each value of n
+            for(int j = 0; j < n; j++){
+               
+                //calculate a + b * 2^j
+                a = a + b; 
+                System.out.print(a + " "); //print the result
+
+                //calculate b * 2^j
+                b = b * 2;
+                
+            }
+            
+            System.out.println("");
+            
 
         }
 
